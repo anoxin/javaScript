e05374c53f540bf22f37a013b776a756b3488626
Anton Anokhin <anoxin.toscha1@yandex.ru>
2022-03-25 22:29:16 +0300

    изменение файла script.js согласно уроку 2

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+const title = 'lesson02';
+const screens = 'Простые, Сложные, Интерактивные';
+const screenPrice = 100;
+const rollback = 50;
+const fullPrice = 50000000;
+const adaptive = true;
+
+console.log(title)
+console.log(screens)
+console.log(adaptive)
+console.log(screens.length)
+console.log('Стоимость верстки экранов ' + `${screenPrice}` + ' долларов')
+console.log('Стоимость разработки сайта ' + `${fullPrice}` + ' долларов')
+
+const screensArray = screens.toLowerCase()
+
+console.log(screensArray.split(', '))
+
+const percentageOfRollback = fullPrice * (rollback/100)
+
+console.log( 'Процент отката посреднику за работу ' + `${percentageOfRollback}`)
