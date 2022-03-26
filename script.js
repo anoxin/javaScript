e24b9cf5b0ba8ca027fe2380c3757cf93135fedc
Anton Anokhin <anoxin.toscha1@yandex.ru>
2022-03-26 11:07:34 +0300

    исправление ошибок

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 let num = 266219;
 
-let str = String(num)
+let str = String(num);
 
-const myArray = Array.from(str)
+const myArray = Array.from(str);
 
-const newArray = myArray.map(i=>Number(i))
+const newArray = myArray.map(i=>Number(i));
 
-let sum = 0
+let sum = 0;
 
 newArray.map(function (el) {
-    
-    return sum = sum + el
-})
+    sum = sum + el;
+    return sum;
+});
 
-console.log(sum)
+console.log(sum);
 
-sum = sum ** 3
+sum = sum ** 3;
 
-console.log(sum)
+console.log(sum);
 
-str = String(sum)
+str = String(sum);
 
-str = str.slice(0, 2)
+str = str.slice(0, 2);
 
-num = Number(str)
+num = Number(str);
 
-console.log(num)
+console.log(num);
