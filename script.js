27ec09c5af1ff00799e02551979ad145de3a5479
Anton Anokhin <anoxin.toscha1@yandex.ru>
2022-03-26 11:12:58 +0300

    исправление ошибок

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ const rollback = 50;
 const fullPrice = 50000000;
 const adaptive = true;
 
-console.log(title)
-console.log(screens)
-console.log(adaptive)
-console.log(screens.length)
-console.log('Стоимость верстки экранов ' + `${screenPrice}` + ' долларов')
-console.log('Стоимость разработки сайта ' + `${fullPrice}` + ' долларов')
+console.log(title);
+console.log(screens);
+console.log(adaptive);
+console.log(screens.length);
+console.log('Стоимость верстки экранов ' + `${screenPrice}` + ' долларов');
+console.log('Стоимость разработки сайта ' + `${fullPrice}` + ' долларов');
 
-const screensArray = screens.toLowerCase()
+const screensArray = screens.toLowerCase();
 
-console.log(screensArray.split(', '))
+console.log(screensArray.split(', '));
 
-const percentageOfRollback = fullPrice * (rollback/100)
+const percentageOfRollback = fullPrice * (rollback/100);
 
-console.log( 'Процент отката посреднику за работу ' + `${percentageOfRollback}`)
+console.log( 'Процент отката посреднику за работу ' + `${percentageOfRollback}`);
